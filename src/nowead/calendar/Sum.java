7f4f9acc910347d34ed97da14337a9008d4d9a1d
DW <DW@DESKTOP-KD2PDQC>
2022-12-27 03:22:10 +0900

    3rd commit

---
+++ src/nowead/calendar/Sum.java
@@ -0,0 +1,18 @@
+package nowead.calendar;
+
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		int a, b;
+		Scanner scanner = new Scanner(System.in);
+		String s1, s2;
+		System.out.println("두 수를 입력해주세요");
+		s1 = scanner.next();
+		s2 = scanner.next();
+		a = Integer.parseInt(s1);
+		b = Integer.parseInt(s2);
+		System.out.println("두 수의 합은" + (a + b) + "입니다.");
+	}
+
+}
