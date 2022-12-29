d1cc4f1321f4ac53412373eb049f895c6636a2f7
DW <DW@DESKTOP-KD2PDQC>
2022-12-29 14:49:20 +0900

    implementation of finding max days of the input month

--- src/nowead/calendar/Calendar.java
+++ src/nowead/calendar/Calendar.java
@@ -1,4 +1,5 @@
 package nowead.calendar;
+import java.util.Scanner;
 
 public class Calendar {
 	public static void main(String[] args) {
@@ -9,7 +10,13 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("달을 입력하세요");
+		int month = scanner.nextInt();
 		
+		int[] maxDays = {31, 28, 31,30,31,30,31,31,30,31,30,31};
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
+		scanner.close();
 	}
 
 }
